daba38663ed0585bdaf4692828d5f2f24674cebc
Claire Harris <wishing.engine@gmail.com>
2018-06-28 21:36:00 +0100

    Add more docs and rename repo

--- src/bin/part00-triangle.rs
+++ src/bin/part00-triangle.rs
@@ -113,7 +113,8 @@ fn main() {
             preserves: &[],
         };
 
-        // TODO: what?
+        // This expresses the dependencies between subpasses. Again, we only have
+        // one subpass for now. Future tutorials may go into more detail.
         let dependency = SubpassDependency {
             passes: SubpassRef::External..SubpassRef::Pass(0),
             stages: PipelineStage::COLOR_ATTACHMENT_OUTPUT..PipelineStage::COLOR_ATTACHMENT_OUTPUT,
@@ -121,7 +122,6 @@ fn main() {
                 ..(Access::COLOR_ATTACHMENT_READ | Access::COLOR_ATTACHMENT_WRITE),
         };
 
-        // TODO: Can you have zero subpasses?
         device.create_render_pass(&[color_attachment], &[subpass], &[dependency])
     };
 
