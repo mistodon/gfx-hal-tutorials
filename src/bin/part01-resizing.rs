e3ec959663679810e5400c3dddff353046ca5710
Claire Harris <wishing.engine@gmail.com>
2018-08-26 14:17:27 +0100

    Run rustfmt and add teapot capability to part 2

--- src/bin/part01-resizing.rs
+++ src/bin/part01-resizing.rs
@@ -2,10 +2,10 @@
 // library crate.
 extern crate gfx_hal_tutorials;
 
-#[cfg(target_os = "macos")]
-extern crate gfx_backend_metal as backend;
 #[cfg(windows)]
 extern crate gfx_backend_dx12 as backend;
+#[cfg(target_os = "macos")]
+extern crate gfx_backend_metal as backend;
 #[cfg(all(unix, not(target_os = "macos")))]
 extern crate gfx_backend_vulkan as backend;
 
